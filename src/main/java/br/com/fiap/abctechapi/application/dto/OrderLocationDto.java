c9a55e14f49a915842e0b1e2a1d82663d0534a94
Daniel Moya da Silva <danimoyasilva.dev@gmail.com>
2023-04-29 17:50:52 -0300

    OrderDto e validações

---
+++ src/main/java/br/com/fiap/abctechapi/application/dto/OrderLocationDto.java
@@ -0,0 +1,21 @@
+package br.com.fiap.abctechapi.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Negative;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class OrderLocationDto {
+    private double latitude;
+    private double longitude;
+    @PastOrPresent
+    private Date date;
+
+}
